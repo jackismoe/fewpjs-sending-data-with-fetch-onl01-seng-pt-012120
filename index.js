f803156c10a36ff680c801c200584587becb17f5
jackismoe <jacksonmo@me.com>
2020-09-01 19:36:31 -0400

    Resubmit

--- index.js
+++ index.js
@@ -1,22 +1,47 @@
-function submitData(name, email) {
-  return fetch('http://localhost:3000/users',{
+const fetchThisCommaDog = () => {
+  let formData = {
+    dogName: 'Ollie',
+    dogBreed: 'Boston Terrier'
+  }
+
+  let configObject = {
+      // trigger catch on line 22 by commenting out the following line
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: JSON.stringify(formData)
+  }
+
+  let dogUrl = 'http://localhost:3000/dogs/'
+
+  fetch(dogUrl, configObject)
+    .then(response => response.json())
+    .then(jsonResponse => console.log(jsonResponse))
+    .catch(error => {
+        alert("Bad Thing!!")
+        console.log(error.message)
+      })
+}
+
+const submitData = (name, email) => {
+  let config = {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
       'Accept': 'application/json'
     },
     body: JSON.stringify({
-        name,
-        email
-      })
+      name,
+      email
     })
-    .then(function(response){
-      return response.json()
-    })
-    .then(function(object){
-      document.body.innerHTML = object['id']
-    })
-    .catch(function(error){
-      document.body.innerHTML = error.message
-    })
+  }
+
+  let userUrl = "http://localhost:3000/users"
+
+  return fetch(userUrl, config)
+    .then(response => response.json())
+    .then(jsonResponse => document.body.innerHTML = jsonResponse['id'])
+    .catch(error => document.body.innerHTML = error.message)
 }
